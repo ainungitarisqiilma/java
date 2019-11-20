58cb069415f12ab2c6994699045c8a6be8f379f6
ainungitarisqiilma <56856942+ainungitarisqiilma@users.noreply.github.com>
2019-11-20 19:35:49 +0700

    Add files via upload

---
+++ encapsulation/src/GetandSet/person.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GetandSet;
+
+/**
+ *
+ * @author S a m s u n g
+ */
+public class person {
+    private String name; 
+
+  public String getName() {
+    return name;
+  }
+
+  
+  public void setName(String newName) {
+    this.name = newName;
+  }
+
+}
