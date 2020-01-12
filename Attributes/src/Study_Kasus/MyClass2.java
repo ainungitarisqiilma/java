7052fc4fd7b0e37fd384c8e3fecbaac52ed90eff
ainungitarisqiilma <56856942+ainungitarisqiilma@users.noreply.github.com>
2020-01-12 13:44:28 +0700

    Add files via upload

---
+++ Attributes/src/Study_Kasus/MyClass2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Study_Kasus;
+
+/**
+ *
+ * @author WINDOWS 10
+ */
+public class MyClass2 {
+  String name = "Ainun";
+  String aname = "Gita";
+  int age = 21;
+  
+  public static void main (String [] args){
+      MyClass2 myObj = new MyClass2();
+      System.out.println ("Name : " + myObj.name + " " + myObj.aname );
+      System.out.println ("Age  : " + myObj.age);
+  }
+}
